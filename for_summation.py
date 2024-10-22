87b034bd7e4da986de40b3696ce5fe29ca23b1b3
PeytonBenedict <benedpey000@alpinesd.org>
2024-10-22 09:26:01 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,6 +1,8 @@
-number = int(input("Enter a number: "))
-total_sum = 0
-for i in range(1, number, + 1):
-  total_sum += i
-
-print("The sum of all the numbers from 1 to", number, "is:", total_sum) 
+number = int(input("Enter a number: "))  
+  
+total_sum = 0  
+  
+for i in range(1, number + 1):  
+    total_sum += i
+  
+print("The sum of numbers from 1 to", number, "is:", total_sum)  
