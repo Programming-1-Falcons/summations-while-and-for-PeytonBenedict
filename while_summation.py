cb3f080597f4925f5a643bdf7dbfde26a73cd1e9
PeytonBenedict <benedpey000@alpinesd.org>
2024-10-24 09:26:36 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,10 +1,14 @@
-number = int(input("Enter a number: "))  
+def sum_numbers(number):  
+    total_sum = 0  
+    current_number = 1  
+      
+    while current_number <= number:  
+        total_sum += current_number     
+        current_number += 1            
+          
+    return total_sum  
   
-total_sum = 0  
-current_number = 1  
-  
-while current_number <= number:  
-    total_sum += current_number   
-    current_number += 1          
- 
-print(total_sum)  
+if __name__ == "__main__":  
+    number = int(input("Enter a number: "))    
+    result = sum_numbers(number)  
+    print(result)  
